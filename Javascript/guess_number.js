7af9eca6b64e34a449c5e86e3f378b05f6e91067
britfitzm <brittanyfitzm99@gmail.com>
2024-02-19 05:39:23 -0800

    new try

--- Javascript/guess_number.js
+++ Javascript/guess_number.js
@@ -74,7 +74,7 @@ function endGame(){
     userInput.setAttribute('disabled', '');
     //Display Start new Game Button
           p.classList.add('btn');
-          p.innerHTML = `<h1 id="New Game">Start New Game</h1>`
+          p.innerHTML = `<h1 id="newGame">Start New Game</h1>`
     startOver.appendChild(p);
     playGame = false;
     newGame();
